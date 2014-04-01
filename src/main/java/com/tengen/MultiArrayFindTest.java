d551d0c8c19a64b03d2724a37d310646843e0ccd
andrzej <andrzej.wudara@gmail.com>
2014-04-01 22:26:54 +0200

    init

---
+++ src/main/java/com/tengen/MultiArrayFindTest.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2008 - 2013 10gen, Inc. <http://10gen.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.tengen;
+
+import com.mongodb.BasicDBList;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
+
+import java.net.UnknownHostException;
+import java.util.Random;
+
+public class MultiArrayFindTest {
+	
+	public static void main(String[] args) throws UnknownHostException {
+		
+		MongoClient client = new MongoClient();
+        DB db = client.getDB("school");
+        DBCollection collection = db.getCollection("students");
+
+        System.out.println("Find one:");
+        DBObject one = collection.findOne();
+        System.out.println(one);
+
+        System.out.println("\nFind all: ");
+        DBCursor cursor = collection.find(new BasicDBObject("scores.type", "homework")).sort(new BasicDBObject("student_id",1).append("score", 1));
+        System.out.println(cursor.count());
+        int i = 0; 
+        try {
+          while (cursor.hasNext()) {
+        	  i++;
+        	  DBObject cur = cursor.next();
+              //System.out.println(cur.get("scores") +" -->" +cur.toMap().toString()  +"-> " +Integer.toString(i) );
+              BasicDBList c = (BasicDBList) cursor.next().get("scores");
+
+              for (Object s : c) {
+            	  System.out.println(s.toString());
+            	  
+			}  
+              
+
+              
+          }
+        } finally {
+            cursor.close();
+        }
+
+        System.out.println("\nCount:");
+        long count = collection.count();
+        System.out.println(count);
+    }
+}
