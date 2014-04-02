dc319c3c04bff98fbcd6030703adcbec974b165a
awandrzej <>
2014-04-02 12:53:49 +0200

    add jsonobject and bsonlist

--- src/main/java/com/tengen/MultiArrayFindTest.java
+++ src/main/java/com/tengen/MultiArrayFindTest.java
@@ -26,7 +26,14 @@ import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 
 import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Random;
+
+import org.bson.BasicBSONObject;
+import org.bson.types.BasicBSONList;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
 public class MultiArrayFindTest {
 	
@@ -41,24 +48,28 @@ public class MultiArrayFindTest {
         System.out.println(one);
 
         System.out.println("\nFind all: ");
-        DBCursor cursor = collection.find(new BasicDBObject("scores.type", "homework")).sort(new BasicDBObject("student_id",1).append("score", 1));
+        DBCursor cursor = collection.find(new BasicDBObject("scores.type", "homework"));
         System.out.println(cursor.count());
         int i = 0; 
         try {
           while (cursor.hasNext()) {
-        	  i++;
-        	  DBObject cur = cursor.next();
-              //System.out.println(cur.get("scores") +" -->" +cur.toMap().toString()  +"-> " +Integer.toString(i) );
-              BasicDBList c = (BasicDBList) cursor.next().get("scores");
-
-              for (Object s : c) {
-            	  System.out.println(s.toString());
-            	  
-			}  
+        	  
+              int id = (Integer) cursor.next().get("_id");
+        	  
+              BasicBSONList bl = (BasicBSONList) cursor.next().get("scores");
+        	  for (Object bo : bl){
+        		  
+        		  BasicBSONObject bo1 = (BasicBSONObject) bo;
+        		  if (bo1.containsValue("homework")){
+        		  System.out.println(Integer.toString(id));
+                  System.out.println(bo1.get("score"));
+        		  }
+        	  }
               
 
-              
-          }
+        	  
+           }
+        
         } finally {
             cursor.close();
         }
@@ -68,3 +79,51 @@ public class MultiArrayFindTest {
         System.out.println(count);
     }
 }
+
+
+/*
+ * how to handle multiple json objects present in a file ? something like this
+
+[{"NAME":"Alan","VALUE":12,"COLOR":"blue","DATE":"Sep. 25, 2009"},
+{"NAME":"Shan","VALUE":13,"COLOR":"green\tblue","DATE":"Sep. 27, 2009"},
+{"NAME":"John","VALUE":45,"COLOR":"orange","DATE":"Sep. 29, 2009"},
+{"NAME":"Minna","VALUE":27,"COLOR":"teal","DATE":"Sep. 30, 2009"}]
+
+How to parse such json file with this library.
+ • Reply•Share › 
+Avatar
+Abhishek  Abhishek • 7 months ago
+OK i figured it out. It's pretty simple
+
+JSONArray jsonarray = (JSONArray)obj;
+
+for (int i=0; i<jsonarray.size(); i++) {
+
+JSONObject jsonObject= (JSONObject)jsonarray.get(i);
+String name = (String) jsonObject.get("name");
+System.out.println(name);
+long age = (Long) jsonObject.get("age");
+System.out.println(age);
+
+// loop array
+JSONArray msg = (JSONArray) jsonObject.get("messages");
+Iterator iterator = msg.iterator();
+while (iterator.hasNext()) {
+System.out.println(iterator.next());	
+}
+
+}
+ * 
+ */
+
+
+/*
+ * 
+ * i++;
+        	  DBObject cur = cursor.next();
+        	  int userId = (Integer) cursor.next().get("_id");
+              BasicDBList c = (BasicDBList) cursor.next().get("scores");
+              for (Object s : c) {
+            	  //System.out.println(Integer.toString(userId));
+            	  System.out.println(s.toString());
+            	  */
